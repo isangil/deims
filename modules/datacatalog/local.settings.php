c29ca2ad2e1b65b4047a786a193ea55588bd4595
corinna.gries@gmail.com <corinna.gries@gmail.com@a2d6d949-e3b6-7b7b-d046-0dff49d5d497>
2012-02-13 20:55:25 +0000

    NTL application for searching datasets and querying and downloading data.

---
+++ modules/datacatalog/local.settings.php
@@ -0,0 +1,12 @@
+<?php
+
+// Custom Research Site groups for location search
+// The following are used by NTL; replace with your own entries
+$info['locations']['AL,BM,CB,CR,FI,ME,MO,SP,TB,TR,WI'] = t('LTER - 11 study lakes');
+$info['locations']['AL,BM,CB,CR,SP,TB,TR'] = t('LTER - 7 northern lakes');
+$info['locations']['FI,ME,MO,WI'] = t('LTER - 4 southern lakes');
+
+
+//
+
+?>
